62743dc0ac94783bf42520597a593a70fff4e40d
Salahiddin <you@example.com>
2024-07-03 17:56:44 +0600

    zadacha

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".nav__burger");
+const modal = document.querySelector("#modal");
+
+function modalToggle() {
+  modal.classList.toggle("modalToggle");
+}
+btn.addEventListener("click", modalToggle);
